b542e486b36f249c477ffb1803357a8f7a47028e
Arthur RICHARD <arthur.richard@protonmail.com>
2024-04-03 22:30:43 +0200

    add sql connection to rds + todo

---
+++ TODO.md
@@ -0,0 +1,5 @@
+# TODO
+
+- Find certificates provider and automate with certbots
+- Find OAuth2 provider and add JWT token to secure routes
+
--- main.go
+++ main.go
@@ -5,13 +5,33 @@ import (
 	"net/http"
 	_ "net/http/pprof"
 	"time"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jmoiron/sqlx"
 )
 
 func greet(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello World! %s", time.Now())
 }
 
+func sqlConnection(password string, host string) error {
+	db, err := sqlx.Connect(
+		"mysql",
+		fmt.Sprintf("admin:%s@tcp(%s:3306)/changelogs", password, host),
+	)
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+
+	return db.Ping()
+}
+
 func main() {
-	http.HandleFunc("/", greet)
-	http.ListenAndServe(":8080", nil)
+	if err := sqlConnection("", "changelogs-db.counfmjnq8zy.eu-west-3.rds.amazonaws.com"); err != nil {
+		println(err)
+	}
+	println("SQL Connection working")
+	http.HandleFunc("/greet", greet)
+	http.ListenAndServe(":8888", nil)
 }
